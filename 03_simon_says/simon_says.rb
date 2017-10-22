18c25f8e4359c9c3f8b4138fae8e1f3f94ce8925
Dan Cecile <dancecile@gmail.com>
2017-10-22 18:59:55 -0400

    Try out string methods

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,34 @@
-#write your code here
+def echo(text)
+  text
+end
+
+def shout(text)
+  text.upcase
+end
+
+def repeat(text, times=2)
+  ([text] * times).join(' ')
+end
+
+def start_of_word(text, length)
+  text[0..(length - 1)]
+end
+
+def first_word(text)
+  text.split(' ').first
+end
+
+def titleize(text)
+  little_words = ['and', 'over', 'the']
+  text
+    .split(' ')
+    .each_with_index
+    .map do |word, i|
+      if i == 0 || !little_words.member?(word)
+        word.capitalize
+      else
+        word
+      end
+    end
+    .join(' ')
+end
