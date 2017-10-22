c8bde11204b54ca5f7bc9d76cb061b05acc2e69e
Dan Cecile <dancecile@gmail.com>
2017-10-22 18:14:12 -0400

    Use a formula for generic temperature conversions

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,38 @@
-#write your code here
+def ftoc(fahrenheit)
+  fahrenheit_to_celsius(fahrenheit)
+end
+
+def ctof(celsius)
+  celsius_to_fahrenheit(celsius)
+end
+
+TemperatureScale = Struct.new(:freezing, :boiling)
+
+def fahrenheit_scale
+  TemperatureScale.new(32.0, 212.0)
+end
+
+def celsius_scale
+  TemperatureScale.new(0.0, 100.0)
+end
+
+def fahrenheit_to_celsius(fahrenheit)
+  convert_temperature(
+    fahrenheit,
+    fahrenheit_scale,
+    celsius_scale)
+end
+
+def celsius_to_fahrenheit(celsius)
+  convert_temperature(
+    celsius,
+    celsius_scale,
+    fahrenheit_scale)
+end
+
+def convert_temperature(from_value, from_scale, to_scale)
+  (from_value - from_scale.freezing) /
+    (from_scale.boiling - from_scale.freezing) *
+    (to_scale.boiling - to_scale.freezing) +
+    to_scale.freezing
+end
