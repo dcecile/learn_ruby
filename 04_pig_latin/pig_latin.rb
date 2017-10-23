5ec882d841915f33f1ebb6d43b1b89eeff16e13b
Dan Cecile <dancecile@gmail.com>
2017-10-22 20:40:40 -0400

    Implement tiny Pig Latin using regular expressions

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,39 @@
-#write your code here
+def translate(text)
+  text
+    .split(' ')
+    .map do |word|
+      retain_punctuation(word, method(:translate_word))
+    end
+    .join(' ')
+end
+
+def retain_punctuation(word, transform)
+  word.sub(/^(.*?)([.,?!])?$/) do
+    "#{transform.call($1)}#{$2}"
+  end
+end
+
+def translate_word(word)
+  original_prefix = find_prefix(word)
+  new_prefix = remove_prefix(word, original_prefix)
+  new_suffix = translate_prefix(original_prefix)
+  "#{new_prefix}#{new_suffix}y"
+end
+
+def find_prefix(text)
+  text[/^(qu|.)*?[aeoiu]/i]
+end
+
+def translate_prefix(prefix)
+  prefix.sub(/.$/, 'a').downcase
+end
+
+def remove_prefix(text, prefix)
+  removed = text[(prefix.length - 1)..-1]
+  capitalize = prefix[0].match(/[A-Z]/)
+  if capitalize
+    removed.capitalize
+  else
+    removed
+  end
+end
--- 04_pig_latin/pig_latin_spec.rb
+++ 04_pig_latin/pig_latin_spec.rb
@@ -22,23 +22,19 @@ require "pig_latin"
 describe "#translate" do
 
   it "translates a word beginning with a vowel" do
-    s = translate("apple")
-    expect(s).to eq("appleay")
+    expect(translate("apple")).to eq("appleay")
   end
 
   it "translates a word beginning with a consonant" do
-    s = translate("banana")
-    expect(s).to eq("ananabay")
+    expect(translate("banana")).to eq("ananabay")
   end
 
   it "translates a word beginning with two consonants" do
-    s = translate("cherry")
-    expect(s).to eq("errychay")
+    expect(translate("cherry")).to eq("errychay")
   end
 
   it "translates two words" do
-    s = translate("eat pie")
-    expect(s).to eq("eatay iepay")
+    expect(translate("eat pie")).to eq("eatay iepay")
   end
 
   it "translates a word beginning with three consonants" do
@@ -46,27 +42,31 @@ describe "#translate" do
   end
 
   it "counts 'sch' as a single phoneme" do
-    s = translate("school")
-    expect(s).to eq("oolschay")
+    expect(translate("school")).to eq("oolschay")
   end
 
   it "counts 'qu' as a single phoneme" do
-    s = translate("quiet")
-    expect(s).to eq("ietquay")
+    expect(translate("quiet")).to eq("ietquay")
   end
 
   it "counts 'qu' as a consonant even when it's preceded by a consonant" do
-    s = translate("square")
-    expect(s).to eq("aresquay")
+    expect(translate("square")).to eq("aresquay")
   end
 
   it "translates many words" do
-    s = translate("the quick brown fox")
-    expect(s).to eq("ethay ickquay ownbray oxfay")
+    expect(translate("the quick brown fox")).to eq("ethay ickquay ownbray oxfay")
   end
 
   # Test-driving bonus:
   # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
   # * retain the punctuation from the original phrase
+
+  it "keeps word capitalization" do
+    expect(translate("A Stitch in Time")).to eq("Aay Itchstay inay Imetay")
+  end
+
+  it "retains original punctuation" do
+    expect(translate("Hey, thanks! OK? No problem.")).to eq("Eyhay, anksthay! Okay? Onay oblempray.")
+  end
 
 end
