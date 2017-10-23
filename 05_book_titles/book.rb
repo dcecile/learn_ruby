b4691adc5292b46b10fa64dda6fd40c1f70cb3a7
Dan Cecile <dancecile@gmail.com>
2017-10-22 20:46:54 -0400

    Make a simple title class

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,24 @@
 class Book
-# write your code here
+  def title=(title)
+    @title = titleize(title)
+  end
+
+  def title
+    @title
+  end
+
+  def titleize(text)
+    little_words = %w(and over the in of a an)
+    text
+      .split(' ')
+      .each_with_index
+      .map do |word, i|
+        if i == 0 || !little_words.member?(word)
+          word.capitalize
+        else
+          word
+        end
+      end
+      .join(' ')
+  end
 end
