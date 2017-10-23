e1553cbcc8b5c8d2765a0303a7f778e0b337a15b
Dan Cecile <dancecile@gmail.com>
2017-10-22 21:11:35 -0400

    Try out simple string format specification

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,35 @@
 class Timer
-  #write your code here
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds=(seconds)
+    @seconds = seconds
+  end
+
+  def seconds
+    @seconds % 60
+  end
+
+  def minutes
+    @seconds / 60 % 60
+  end
+
+  def hours
+    @seconds / 60**2
+  end
+
+  def time_string
+    to_s
+  end
+
+  def to_s
+    [hours, minutes, seconds]
+      .map(&method(:padded))
+      .join(':')
+  end
+
+  def padded(digits)
+    "%02d" % digits
+  end
 end
